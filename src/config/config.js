bd90f03836403cd009590ba4ad7c39de7c689487
Ibrahim Broachwala <ibrahimkb1996@gmail.com>
2017-10-15 14:55:23 +0530

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,8 +1,8 @@
 export const db_config = {
-    apiKey: "AIzaSyBqm0Fqf7VeClS5a2dcVjj77ieSczgefdk",
-    authDomain: "reactfire-notes.firebaseapp.com",
-    databaseURL: "https://reactfire-notes.firebaseio.com",
-    projectId: "reactfire-notes",
-    storageBucket: "reactfire-notes.appspot.com",
-    messagingSenderId: "358953270495"
+    apiKey: "<your api key>",
+    authDomain: "<projectname>.firebaseapp.com",
+    databaseURL: "https://<projectname>.firebaseio.com",
+    projectId: "<projectid>",
+    storageBucket: "<projectid>.appspot.com",
+    messagingSenderId: "<your id>"
   };
